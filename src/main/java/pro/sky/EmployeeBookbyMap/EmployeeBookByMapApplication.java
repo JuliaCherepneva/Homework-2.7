16c2745c4fd1472aff7b46c0ee3879e228310ad1
JuliaCherepnev <yuliya.cherepnev@mail.ru>
2024-07-24 20:35:41 +0400

    Первый коммит на спринге

---
+++ src/main/java/pro/sky/EmployeeBookbyMap/EmployeeBookByMapApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.EmployeeBookbyMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeBookByMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeBookByMapApplication.class, args);
+	}
+
+}
